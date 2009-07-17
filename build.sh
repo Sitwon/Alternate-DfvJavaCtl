e69eed627709508fbed932c8e5e80606d0d725e4
Ben Mendis <dragonwisard@gmail.com>
2009-07-17 11:09:11 -0400

    A build script to generate Java 1.4 JAR files

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e # Exit on errors
+cd src/
+for i in jp/co/antenna/DfvJavaCtl/*.java
+do
+	javac -source 1.4 -target 1.4 $i
+done
+jar cf ../DfvJavaCtl_1.4.jar jp/co/antenna/DfvJavaCtl/*.class
+rm jp/co/antenna/XfoJavaCtl/*.class
